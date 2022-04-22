16e943381d5705a18df68d110fde2c1bb57a2955
Lucas Araujo <lucas.araujo@atlastechnol.com>
2022-04-22 14:43:31 -0300

    fix: change extension file name and fix bash,sh

--- syntax-analisys/bash.sh
+++ syntax-analisys/bash.sh
@@ -8,27 +8,34 @@ done
 
 echo ========= JAVACC =========
 javacc Lugosi.jj;
+create_whitelines 3
 
 echo ========= JAVAC =========
 javac *.java;
-echo ========= JAVAC COMPLETED =========
-
 create_whitelines 3
+echo ========= JAVAC COMPLETED =========
+create_whitelines 3
+
 echo ========= INITIATING TESTS =========
 create_whitelines 3
+
 echo ========= EXAMPLE =========
-java Lugosi example.sim;
+java Lugosi example.lug;
 create_whitelines 2
+
 echo ========= TEST 1 =========
-java Lugosi example1.sim;
+java Lugosi example1.lug;
 create_whitelines 2
+
 echo ========= TEST 2 =========
-java Lugosi example2.sim;
+java Lugosi example2.lug;
 create_whitelines 2
+
 echo ========= TEST 3 =========
-java Lugosi example3.sim;
+java Lugosi example3.lug;
 create_whitelines 2
-echo Deleting compiled files
+
+echo ========= Deleting compiled files... =========
 rm -rf *.java
 rm -rf *.class
 create_whitelines 2
